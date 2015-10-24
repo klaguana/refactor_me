3811022500c2ed97f3a84ffa600afddff7e7367d
klaguana <klaguana@hawaii.edu>
2015-10-23 18:42:16 -1000

    deleted dumb extra spaces

--- get_avatar.py
+++ get_avatar.py
@@ -10,13 +10,13 @@ import shutil
 # PARSE COMMAND LINE ARGUMENTS
 
 parser = argparse.ArgumentParser()
-parser.add_argument(  'username')
+parser.add_argument('username')
 args = parser.parse_args()
 
 # CALL THE GITHUB API AND GET USER INFO
 
 request_url = 'https://api.github.com/users/' + args.username
-result = requests.get( RequestUrl )
+result = requests.get(RequestUrl)
 if result.ok :
     user_info = json.loads(result.content)
     avatarURL = user_info['avatar_url']
@@ -26,7 +26,7 @@ else:
 
 # DOWNLOAD AND SAVE IMAGE FILE
 
-L = requests.get(avatarURL , stream=True)
+L = requests.get(avatarURL, stream=True)
 if L.ok:
-    with open(args.username + '.png' , 'wb') as OuTfIle:
-        shutil.copyfileobj( L.raw,  OuTfIle )
+    with open(args.username + '.png', 'wb') as OuTfIle:
+        shutil.copyfileobj( L.raw, OuTfIle )
